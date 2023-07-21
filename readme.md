b146472d615807be5295a567c7e1aa3e1b947ec5
9names <60134748+9names@users.noreply.github.com>
2023-07-22 08:34:06 +1000

    Add panic handler, testing i2c

---
+++ readme.md
@@ -0,0 +1,7 @@
+To run:
+
+```
+cargo objcopy --release -- -O ihex project.hex
+```
+
+then start wokwi in vscode
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
 #![no_std]
 #![no_main]
 #![feature(type_alias_impl_trait)]
+#![feature(panic_info_message)]
 
 use embassy_executor::Spawner;
-use embassy_rp::gpio;
-use embassy_rp::uart;
+use embassy_rp::{gpio, uart, i2c};
 use embassy_time::{Duration, Timer};
 use gpio::{Level, Output};
-use panic_probe as _;
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
+use core::panic::PanicInfo;
+use heapless::String;
+
+#[panic_handler]
+fn panic(info :&PanicInfo) -> !{
+    // conjure a new copy of the peripherals - we've already panic'd so no-one should mind
+    let p: embassy_rp::Peripherals = unsafe {core::mem::transmute(())};
+    // set the LED high. hopefully this is a good enough sign that things are not good
+    let mut led = Output::new(p.PIN_25, Level::High);
+    let config = uart::Config::default();
+    let mut uart = uart::Uart::new_blocking(p.UART0, p.PIN_0, p.PIN_1, config);
+    use core::fmt::Write;
+    let mut text: String<1024> = String::new();
+    if let Some(message) = info.message() {
+        let _ = writeln!(&mut text, "{}\r\n", message);
+        let _ = uart.blocking_write(text.as_bytes());
+        let _ = uart.blocking_flush();
+    }
+    loop{
+        cortex_m::asm::delay(10000000);
+        led.set_low();
+        cortex_m::asm::delay(10000000);
+        led.set_high();
+    }
+}
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -15,9 +40,22 @@ async fn main(_spawner: Spawner) {
     let config = uart::Config::default();
     let mut uart = uart::Uart::new_blocking(p.UART0, p.PIN_0, p.PIN_1, config);
     let mut led = Output::new(p.PIN_25, Level::Low);
+
     uart.blocking_write("Program start!\r\n".as_bytes())
         .unwrap();
 
+    // panic!("at the disco");
+
+    let scl = p.PIN_9;
+    let sda = p.PIN_8;
+    let mut config = i2c::Config::default();
+    config.frequency = 400_000;
+    let i2c = i2c::I2c::new_blocking(p.I2C0, scl, sda, config);
+
+    // let interface = I2CDisplayInterface::new(i2c);
+    // let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0).into_terminal_mode();
+    //display.init().unwrap();
+    // let _ = display.print_char('a');
     loop {
         led.set_high();
         uart.blocking_write("LED on\r\n".as_bytes()).unwrap();
