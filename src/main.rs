4ee2b720fcec20dff73ca0e6e4d19ed68daf722c
9names <60134748+9names@users.noreply.github.com>
2023-07-21 13:07:17 +1000

    Test blocking UART

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 
 use embassy_executor::Spawner;
 use embassy_rp::gpio;
+use embassy_rp::uart;
 use embassy_time::{Duration, Timer};
 use gpio::{Level, Output};
 use panic_probe as _;
@@ -11,13 +12,19 @@ use panic_probe as _;
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
+    let config = uart::Config::default();
+    let mut uart = uart::Uart::new_blocking(p.UART0, p.PIN_0, p.PIN_1, config);
     let mut led = Output::new(p.PIN_25, Level::Low);
+    uart.blocking_write("Program start!\r\n".as_bytes())
+        .unwrap();
 
     loop {
         led.set_high();
+        uart.blocking_write("LED on\r\n".as_bytes()).unwrap();
         Timer::after(Duration::from_secs(1)).await;
 
         led.set_low();
+        uart.blocking_write("LED off\r\n".as_bytes()).unwrap();
         Timer::after(Duration::from_secs(1)).await;
     }
 }
